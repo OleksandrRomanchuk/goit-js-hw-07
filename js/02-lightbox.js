5644d00b81e958bad9855a7515c24bdfb049c06d
Oleksandr Romanchuk <novator0511@gmail.com>
2022-10-24 15:31:11 +0300

    task#2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+galleryContainer.addEventListener('click', showLargeImg);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+function createGalleryMarkup(data) {
+    return data
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}" />
+                    </a>`;
+        })
+        .join('');
+}
+
+function showLargeImg(event) {
+    event.preventDefault();
+
+    if (!validateEventNode(event)) return;
+}
+
+function validateEventNode(event) {
+    return event.target.nodeName === 'IMG' ? true : false;
+}
