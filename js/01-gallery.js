ede542c9023762be02be6c63c6c94b68b2a6e4c3
Oleksandr Romanchuk <novator0511@gmail.com>
2022-10-24 14:27:45 +0300

    task#1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+galleryContainer.addEventListener('click', showLargeImg);
+
+function createGalleryMarkup(data) {
+    return data
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`;
+        })
+        .join('');
+}
+
+function showLargeImg(event) {
+    event.preventDefault();
+
+    if (!validateEventNode(event)) return;
+
+    createLargeImgMarkup(event);
+}
+
+function validateEventNode(event) {
+    return event.target.nodeName === 'IMG' ? true : false;
+}
+
+function createLargeImgMarkup(data) {
+    const instance = basicLightbox.create(`<img
+                                            class="gallery__image"
+                                            src="${event.target.dataset.source}"
+                                            alt="${event.target.alt}"
+                                            />`);
+
+    instance.show(closeWhenEscapePressed(instance));
+}
+
+function closeWhenEscapePressed(el) {
+    document.addEventListener('keydown', event => {
+        return event.code === 'Escape' ? el.close() : false;
+    });
+}
